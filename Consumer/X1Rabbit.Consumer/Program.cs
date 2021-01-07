d1865119bf3b245afd9e6646cc43bae8bd9e7f17
Md Risul Karim <risulkarimx1@gmail.com>
2021-01-07 01:03:39 +0100

    first commit with producer and consumer

---
+++ Consumer/X1Rabbit.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace X1Rabbit.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queName = "demo-q";
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (o, eventArgs) => OnMessageReceieved(o, eventArgs);
+            channel.BasicConsume(queName, true, consumer);
+            Console.ReadLine();
+        }
+
+        private static void OnMessageReceieved(object? sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"message found : {message}");
+        }
+    }
+}
---
+++ Producer/X1Rabbit.Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace X1Rabbit.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queName = "demo-q";
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            int c = 0;
+
+            while (true)
+            {
+                var key =  Console.ReadKey();
+                if(key.Key == ConsoleKey.Escape) break;
+
+                var message = new { Name = "Producer", Message = $"Hello {c++}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                channel.BasicPublish("", queName, null, body);
+            }
+
+            
+
+        }
+    }
+}
